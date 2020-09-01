50e0521f4a3d2184bf98bf49c8696a27b56d6978
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-09-02 00:13:28 +0500

    Initialize app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className=" app app-amazon">
+      <div className="app-amazon__header"></div>
+      <div className="app-amazon__body"></div>
+      <div className="app-amazon__footer"></div>
+    </div>
+  );
+};
+
+export default App;
