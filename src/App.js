64e9b480ece1e7e3dcff3c775dd19d28ca6227ff
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-09-06 05:15:15 +0500

    Added components to root component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.scss";
+import NavBar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Main from "./components/main/Main";
+import BannerComponent from "./components/banner/Banner";
 
 const App = () => {
   return (
-    <div className=" app app-amazon">
-      <div className="app-amazon__header"></div>
-      <div className="app-amazon__body"></div>
-      <div className="app-amazon__footer"></div>
+    <div className="app app-amazon">
+      <div className="app-amazon__header">
+        <NavBar></NavBar>
+      </div>
+      <div className="app-amazon__body">
+        <BannerComponent
+          text="We are delivering to your region with limited shipping options. Please expect extended delivery time."
+          cta={"learn more"}
+        ></BannerComponent>
+        <Main></Main>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
