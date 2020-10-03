7979d04320efca75a665007bc3e447caf4494478
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-10-03 21:12:12 +0500

    App cleanup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.scss";
 import NavBar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Main from "./components/main/Main";
 import BannerComponent from "./components/banner/Banner";
 import { connect } from "react-redux";
-import { setProducts } from "./store/platform/actions";
-import { fetchProducts } from "./services/products";
 
-const App = ({ setProducts }) => {
-  useEffect(() => {
-    loadProducts();
-  }, []);
-
-  const loadProducts = async () => {
-    const data = await fetchProducts();
-    setProducts(data);
+const App = ({}) => {
+  const renderBanner = () => {
+    return (
+      <BannerComponent
+        text="We are delivering to your region with limited shipping options. Please expect extended delivery time."
+        cta={"learn more"}
+      ></BannerComponent>
+    );
   };
+
+  const renderNavbar = () => {
+    return <NavBar></NavBar>;
+  };
+
   return (
     <div className="app app-amazon">
-      <div className="app-amazon__header">
-        <NavBar></NavBar>
-      </div>
+      <div className="app-amazon__header">{renderNavbar()}</div>
+      {renderBanner()}
       <div className="app-amazon__body">
-        <BannerComponent
-          text="We are delivering to your region with limited shipping options. Please expect extended delivery time."
-          cta={"learn more"}
-        ></BannerComponent>
         <Main></Main>
       </div>
       <Footer></Footer>
@@ -34,11 +32,6 @@ const App = ({ setProducts }) => {
   );
 };
 
-const connection = connect(
-  null,
-  {
-    setProducts
-  }
-);
+const connection = connect(null, {});
 
 export default connection(App);
