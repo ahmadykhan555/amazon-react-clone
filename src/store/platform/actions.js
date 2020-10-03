8f2effde49c774015262d712b3d69ae82f0b2323
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-10-03 21:11:57 +0500

    Added store actions

--- src/store/platform/actions.js
+++ src/store/platform/actions.js
@@ -1,16 +1,4 @@
-const platforms = ["apple", "firebase", "nike", "adidas"];
-
-export const togglePlatform = () => ({
-  type: "toggle",
-  payload: platforms[Math.floor(Math.random() * (platforms.length - 1))]
-});
-
-export const incrementCounter = () => ({
-  type: "inc",
-  payload: Math.floor(Math.random() * 100 + 1000)
-});
-
-export const setProducts = products => ({
+export const setProducts = (products) => ({
   type: "set_products",
-  payload: products
+  payload: products,
 });
